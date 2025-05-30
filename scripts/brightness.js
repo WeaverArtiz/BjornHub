badeb26e831729d414f5b51d4d1546ab4c0d49f5
WeaverArtiz <spawlo112@gmail.com>
2025-05-30 15:58:22 +0200

    improved Light/Dark mode handling
    Added light and dark mode to buttons.
    Made transition between the two smoother, more eye-pleasing.

--- scripts/brightness.js
+++ scripts/brightness.js
@@ -1,11 +1,3 @@
 function toggleTheme() {
-  const themeLink = document.getElementById('themeStylesheet');
-  const currentTheme = themeLink.getAttribute('href');
-
-  if (currentTheme === 'styles/light.css') {
-    themeLink.setAttribute('href', 'styles/style.css');
-  } 
-  else{
-    themeLink.setAttribute('href', 'styles/light.css');
-  }
+  document.body.classList.toggle('light');
 }
